ab3f8add2c74c36c8b22bbaba89c98728749ddb2
Daniela González Muñiz <danielagm44@educastur.es>
2025-11-21 19:56:11 +0100

    pequeño cambio

--- src/main/java/com/example/ExamenMocDGM/controller/ProductoController.java
+++ src/main/java/com/example/ExamenMocDGM/controller/ProductoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
+
 @RestController
 @RequestMapping(path="/examenMocDGM")
 public class ProductoController {
