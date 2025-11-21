80ee1979b42004c5f191b999f11dd79c428d387c
Daniela González Muñiz <danielagm44@educastur.es>
2025-11-21 17:24:30 +0100

    Initial commit

---
+++ src/main/java/com/example/ExamenMocDGM/ExamenMocDgmApplication.java
@@ -0,0 +1,13 @@
+package com.example.ExamenMocDGM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenMocDgmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenMocDgmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ExamenMocDGM/ExamenMocDgmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ExamenMocDGM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenMocDgmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
